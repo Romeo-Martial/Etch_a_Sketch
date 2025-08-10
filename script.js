530f34241ce36d85f7116b5498b914866400d067
Romeo-Martial <104857205+Romeo-Martial@users.noreply.github.com>
2025-08-10 18:51:16 +0200

    Add a button to index.html to prompt the user for a square size
    Implement a main function to handle the game and
    a makeGrid function to build the Grid
    
    Styling the button and the background of its container

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-// Create a 16x16 grid of square divs.
-// create 256 div elements inside div with id "container"
-const container = document.getElementById("container");
-for (let i = 0; i < 256; i++) {
-  const divItem = document.createElement("div");
-  container.appendChild(divItem);
-  divItem.addEventListener("mouseenter", () => {
-    divItem.style.backgroundColor = "black";
+function main() {
+  // Add a popup asking the number of square per side
+  const newGridBtn = document.getElementById("newGridBtn");
+  newGridBtn.addEventListener("click", () => {
+    const squaresPerSide = prompt(
+      "Enter number of squares per side (mawimum 100): "
+    );
+    // create the grid if the user enters a number between 1 and 100
+    if (
+      squaresPerSide !== null &&
+      squaresPerSide > 0 &&
+      squaresPerSide <= 100
+    ) {
+      console.log(`User wants ${squaresPerSide} squares per side`);
+      makeGrid(squaresPerSide);
+    }
   });
 }
+
+// Implement a function to make the grid
+function makeGrid(size) {
+  const container = document.getElementById("container_2");
+  container.innerHTML = ""; // clear old grid
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div");
+    square.style.width = `${100 / size}vw`;
+    square.style.height = `${100 / size}vw`;
+    container.appendChild(square);
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor = "black";
+    });
+  }
+}
+
+main();
